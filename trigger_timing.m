ab5eaf25369996b7bd56f248b6190092eb246888
Alix Battison <alexandria.battison@uconn.edu>
2017-04-11 09:40:56 -0400

    Update trigger_timing.m

--- trigger_timing.m
+++ trigger_timing.m
@@ -3,14 +3,14 @@
 %Intan board has a minimum of 1 second buffer, plus some variable amount
 %
 %Author: Alix Battison. 3/17/2017
-
+data1 = board_adc_data(1,:);
 data = data1;
 sampling_rate = 20000; %change according to sampling rate used during data acquisition
-len = length(data(:,1));
+len = length(data(1,:));
 
 i = 1;
 for i = 1:len
-    if data(i,1) < max(data(:,1))
+    if data(1,i) < max(data(1, : ))
      i = i+1;
         
     else 
@@ -25,4 +25,6 @@ sec = i/sampling_rate;
 vardel = sec - 1;
 disp(sprintf('Rising edge occurs after %f seconds and %d data points.', sec, i))
 %disp(sprintf('Variable delay of %d sec after intan 1 second delay', vardel))
+
+
 
